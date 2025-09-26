2d6bb57a640fd087f9de0dbe581f0e0ecaeea0ad
hzhilong <1107495265@qq.com>
2025-09-26 15:08:56 +0800

    完善 useLoadingData 组合式函数；

--- src/composables/useLoadingData.ts
+++ src/composables/useLoadingData.ts
@@ -2,13 +2,26 @@ import { ref } from 'vue'
 
 export const useLoadingData = () => {
   const loading = ref(false)
-  const loadingData = async <T=void>(doneFn: (() => T | Promise<T>) | Promise<T>): Promise<T> => {
-    loading.value = true
-    try {
-      // 统一转换成 Promise
-      return typeof doneFn === 'function' ? await doneFn() : await doneFn
-    } finally {
-      loading.value = false
+
+  /**
+   * 包装函数或 Promise，使其执行时自动管理 loading 状态
+   * 支持同步函数、异步函数、直接 Promise，支持参数传入
+   */
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const loadingData = <TArgs extends any[] = [], TReturn = void>(
+    doneFn: ((...args: TArgs) => TReturn | Promise<TReturn>) | Promise<TReturn>
+  ): ((...args: TArgs) => Promise<TReturn>) => {
+    return async (...args: TArgs): Promise<TReturn> => {
+      loading.value = true
+      try {
+        if (typeof doneFn === 'function') {
+          return await doneFn(...args)
+        } else {
+          return await doneFn
+        }
+      } finally {
+        loading.value = false
+      }
     }
   }
 
