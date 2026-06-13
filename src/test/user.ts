4854f039726596c78de2b5b93ddc5406eff5e90a
hzhilong <hzhilong777@outlook.com>
2026-06-14 04:28:31 +0800

    0.0.2

--- src/test/user.ts
+++ src/test/user.ts
@@ -3,10 +3,10 @@ import type { UserInfo, UserInfoWithCookie } from '@ybgnb/bili-api'
 export const testUser = {
   mid: 1234567890,
   name: '测试用户测试用户测试用户测试用户测试用户',
-  face: 'https://i1.hdslb.com/bfs/face/5a69ba7f2382166397785571cad2c7eaf54cb2a9.jpg@150w_150h.jpg',
+  face: 'http://i0.hdslb.com/bfs/face/member/noface.jpg',
   sex: '男',
   level: 6,
-  sign: '富强、民主、文明、和谐、自由、平等、公正、法治、爱国、敬业、诚信、友善',
+  sign: '签名签名签名签名签名签名签名签名签名',
   moral: 0,
   silence: 0,
   following: 666,
